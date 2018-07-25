54c079d6e1c1ee9d87d7f782cb9a251fa41e15f9
Kurbatov14500 <Kurbatov14500@gmail.com>
2018-07-25 20:55:34 +0300

    написал фигни

---
+++ ConsoleApp2/Human.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Human
+    {
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+
+                }
+            }
+        }
+    }
+}
